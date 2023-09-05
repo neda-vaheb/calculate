32023eb9d67660161ed5d96fbdfa8f2a355805c5
Neda Vaheb <n.vaheb.nv@gmail.com>
2023-09-05 04:09:57 -0700

    the classes is add to javascript file

---
+++ main.js
@@ -0,0 +1,5 @@
+const input = document.querySelector(".display");
+const showDisplay = document.querySelector(".show-display");
+const allClear = document.querySelector(".all-clear");
+const lastClear = document.querySelector(".last-clear");
+const calculate = document.querySelector(".calculate");
