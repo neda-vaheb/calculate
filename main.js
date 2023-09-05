b717829528ecdbd1ca1416b80f6dd70c562200de
Neda Vaheb <n.vaheb.nv@gmail.com>
2023-09-05 04:31:37 -0700

    file javascript had some changes

--- main.js
+++ main.js
@@ -2,41 +2,37 @@ let displayBox = document.querySelector(".display");
 
 function showDisplay(event){
     const x = event.target.innerText;
-   if(displayBox.innerHTML === 0 ){
-    return displayBox.innerText = x;
-
-   }else{
-    return displayBox.innerText +=x;
-   }
+    if(displayBox.innerHTML == 0){
+        return displayBox.innerText = x;
+    }else{
+        return displayBox.innerText += x;
+    }
 }
 function calculate(){
     let result = displayBox.innerText;
-    displayBox.innerText = evel(result);
-
+   displayBox.innerText= eval(result);
 }
-
 function allClear(){
-displayBox.innerText =0;
+    displayBox.innerText = 0;
 }
-
 function lastClear(){
     let number = displayBox.innerText;
     if(number.length === 1){
-        displayBox.innerHTML = 0;
+        displayBox.innerText = 0;
     }else{
-        displayBox.innerText = number.substring(0 , number.length-1);
+        displayBox.innerText = number.substring(0 , number.length - 1);
     }
 
 }
-let displayBtn = document.querySelector(".show-display");
-const allClear = document.querySelector(".all-clear");
-const lastClear = document.querySelector(".last-clear");
-const calculate = document.querySelector(".calculate");
-allClear.addEventListener("click",allClear);
-lastClear.addEventListener("click",lastClear);
-calculate.addEventListener("click",calculate);
-displayBtn.forEach(item => {
-    item.addEventListener("click",showDisplay)
+
+
+
+document.querySelector(".last-clear").addEventListener("click",lastClear);
+document.querySelector(".all-clear").addEventListener("click",allClear);
+document.querySelector(".calculate").addEventListener("click",calculate);
+let showBtn = document.querySelectorAll(".show-display");
+showBtn.forEach(item => {
+    item.addEventListener("click",showDisplay);
     
 });
 
